a32b753021485ca5d0449cd38609ad021d19c165
zznixt07 <zznixt07@protonmail.com>
2021-11-01 10:52:30 +0545

    inital upload

---
+++ src/bowser/main.py
@@ -0,0 +1,114 @@
+import ssl
+import socket
+from typing import Dict, List, Literal, Optional, TextIO, Tuple
+
+HTTP_VER: str = '1.1'
+SUPPORTED_URLS: Tuple[str, ...] = (
+    'http',
+    'https',
+    'file',
+)  # keep tuple for typing.Literal
+Scheme = Literal[SUPPORTED_URLS]
+CTX: ssl.SSLContext = ssl.create_default_context()
+
+
+def url_parse(url: str) -> Tuple[Scheme, int, str, str]:
+    scheme: Scheme
+    port: int
+    host: str
+    path: str
+    scheme, url = url.split('://', 1)
+    assert scheme in SUPPORTED_URLS, f'Unknown scheme {scheme}'
+    splitted: List[str] = url.split('/', 1)
+    host_and_port: List[str] = splitted[0].split(':', 1)
+    if len(host_and_port) == 2:
+        # say if splitted=['example.org:666', 'path.html'], remove port from 0th index
+        splitted[0] = host_and_port[0]
+        port = int(host_and_port[1])
+    else:
+        port = 80 if scheme == 'http' else 443
+    if len(splitted) == 1:
+        host = splitted[0]
+        path = ''
+    else:
+        host, path = splitted
+    path = '/' + path
+    return scheme, port, host, path
+
+
+def request(
+    url: str, headers: Optional[Dict[str, str]] = None
+) -> Tuple[Dict[str, str], str]:
+    scheme: Scheme
+    port: int
+    host: str
+    path: str
+    scheme, port, host, path = url_parse(url)
+    if headers is None:
+        headers = {}
+    headers.update(
+        {'Host': host, 'Connection': 'close', 'User-Agent': 'pybrowser, like Chrome'}
+    )
+    sock: socket.socket
+    response: TextIO
+    with socket.socket(
+        family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
+    ) as sock:
+        sock.connect((host, port))
+        reqs: str = (
+            f'GET {path} HTTP/{HTTP_VER}\r\n'
+            + '\r\n'.join([f'{k}: {v}' for k, v in headers.items()])
+            + '\r\n'
+            + '\r\n'
+        )
+        if scheme == 'https':
+            with CTX.wrap_socket(sock, server_hostname=host) as ssock:
+                ssock.sendall(reqs.encode('utf-8'))
+                response = ssock.makefile('r', encoding='UTF-8', newline='\r\n')
+        else:
+            sock.sendall(reqs.encode('utf-8'))
+            response = sock.makefile('r', encoding='UTF-8', newline='\r\n')
+
+    status_line: str = response.readline()
+    version: str
+    status: str
+    explanation: str
+    version, status, explanation = status_line.split(' ', 2)
+    assert status == '200', f'{status}: {explanation}'
+    resp_headers: Dict[str, str] = {}
+    while True:
+        line: str = response.readline()
+        if line == '\r\n':
+            break
+        header: str
+        value: str
+        header, value = line.split(':', 1)
+        resp_headers[header.lower()] = value.strip()
+
+    body: str = response.read()
+    return resp_headers, body
+
+
+def show(body: str):
+    in_angle: bool = False
+    c: str
+    for c in body:
+        if c == '<':
+            in_angle = True
+        elif c == '>':
+            in_angle = False
+        elif not in_angle:
+            print(c, end='')
+
+
+def load(url: str) -> None:
+    show(request(url)[1])
+
+
+if __name__ == '__main__':
+    url: str = 'https://example.org'
+    # url: str = 'http://example.org'
+    headers: Dict[str, str]
+    body: str
+    headers, body = request(url)
+    show(body)
---
+++ tests/test_main.py
@@ -0,0 +1,52 @@
+from src.bowser.main import url_parse
+
+
+def test_url_parse_scheme():
+    index: int = 0
+    assert url_parse('http://example.org')[index] == 'http'
+    assert url_parse('https://example.org')[index] == 'https'
+    # assert url_parse('file:///C:\\Users\\zznixt\\OneDrive\\innit_perhaps\\2nd time\\browser-engineering\\bowser\\home.html')[index] == 'file'
+    # assert url_parse('data://example.org')[index] == 'data'
+
+
+def test_url_parse_port():
+    index: int = 1
+    assert url_parse('http://example.org')[index] == 80
+    assert url_parse('https://example.org')[index] == 443
+    assert url_parse('https://api.example.org:5645')[index] == 5645
+
+
+def test_url_parse_host():
+    index: int = 2
+    assert url_parse('http://example.org')[index] == 'example.org'
+    assert url_parse('https://example.org')[index] == 'example.org'
+    assert url_parse('https://api.example.org')[index] == 'api.example.org'
+    assert url_parse('https://example.org/home.html')[index] == 'example.org'
+
+
+def test_url_parse_path():
+    index: int = -1
+    assert url_parse('http://example.org')[index] == '/'
+    assert url_parse('https://api.example.org')[index] == '/'
+    assert url_parse('https://example.org/home.html')[index] == '/home.html'
+    assert (
+        url_parse('https://example.org/home.html?k1=v#i54')[index]
+        == '/home.html?k1=v#i54'
+    )
+
+
+def test_url_parse():
+    assert url_parse('http://example.org') == ('http', 80, 'example.org', '/')
+    assert url_parse('https://example.org') == ('https', 443, 'example.org', '/')
+    assert url_parse('https://api.example.org') == (
+        'https',
+        443,
+        'api.example.org',
+        '/',
+    )
+    assert url_parse('https://example.org/page.html?k=v#id1') == (
+        'https',
+        443,
+        'example.org',
+        '/page.html?k=v#id1',
+    )
